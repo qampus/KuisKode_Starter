de5d0d9fbd87c889ae04ec8b774d414eee5d8128
Hasan <hasan.shobri@outlook.com>
2017-10-06 21:44:38 +0700

    first commit

--- application/models/Tebakkode_m.php
+++ application/models/Tebakkode_m.php
@@ -1,34 +1,80 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Tebakkode_m extends CI_Model {
+class Tebakkode_m extends CI_Model
+{
 
-  function __construct(){
-    parent::__construct();
-    $this->load->database();
-  }
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
 
-  // Events Log
-  function log_events($signature, $body)
-  {
-    $this->db->set('signature', $signature)
-    ->set('events', $body)
-    ->insert('eventlog');
+    // Events Log
+    function log_events($signature, $body)
+    {
+        $this->db->set('signature', $signature)
+            ->set('events', $body)
+            ->insert('eventlog');
 
-    return $this->db->insert_id();
-  }
+        return $this->db->insert_id();
+    }
 
-  // Users
-  function getUser($userId){}
+    // Users
+    function getUser($userId)
+    {
+        $data = $this->db->where('user_id', $userId)->get('users')->row_array();
+        if (count($data) > 0) return $data;
+        return false;
+    }
 
-  function saveUser($profile){}
+    function saveUser($profile)
+    {
+        $this->db->set('user_id', $profile['userId'])
+            ->set('display_name', $profile['displayName'])
+            ->insert('users');
 
-  // Question
-  function getQuestion($questionNum){}
+        return $this->db->insert_id();
+    }
 
-  function isAnswerEqual($number, $answer){}
+    // Question
+    // Question
+    function getQuestion($questionNum)
+    {
+        $data = $this->db->where('number', $questionNum)
+            ->get('questions')
+            ->row_array();
 
-  function setUserProgress($user_id, $newNumber){}
+        if (count($data) > 0) return $data;
+        return false;
+    }
 
-  function setScore($user_id, $score){}
+    function isAnswerEqual($number, $answer)
+    {
+        $this->db->where('number', $number)
+            ->where('answer', $answer);
+
+        if (count($this->db->get('questions')->row()) > 0)
+            return true;
+
+        return false;
+    }
+
+    function setUserProgress($user_id, $newNumber)
+    {
+        $this->db->set('number', $newNumber)
+            ->where('user_id', $user_id)
+            ->update('users');
+
+        return $this->db->affected_rows();
+    }
+
+    function setScore($user_id, $score)
+    {
+        $this->db->set('score', $score)
+            ->where('user_id', $user_id)
+            ->update('users');
+
+        return $this->db->affected_rows();
+    }
 
 }
